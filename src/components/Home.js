a5371fd787dce23d93bdccbec37f8e642032ef71
JSubba <prolearn1000@protonmail.com>
2022-05-13 19:42:49 +0900

    modifications made

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,10 @@ const Home = () => {
       className="bg-gray-900 bg-right sm:bg-center bg-cover bg-no-repeat text-gray-300 w-full h-screen"
     >
       <div className="max-w-[55rem] mx-auto px-8 -m-8 flex flex-col justify-center h-full">
-        <h1 className="text-4xl text-cyan-500 sm:text-5xl font-bold">
+        <h1 className="text-8xl text-cyan-500 sm:text-6xl font-bold">
           J.Subba
         </h1>
-        <h2 className="text-5xl sm:text-6xl font-bold py-4">
+        <h2 className="text-10xl sm:text-8xl font-bold py-4">
           I'm a Web Developer
         </h2>
         <p className="py-4 max-w-md">
@@ -22,7 +22,7 @@ const Home = () => {
         </p>
         <div>
           <Link to="projects" smooth={true} duration={500}>
-            <button className="px-6 py-3 my-2 flex items-center shadow-md shadow-cyan-500 hover:text-cyan-500 hover:scale-105 ease-in duration-500 font-semibold">
+            <button className="px-10 py-6 my-2 flex items-center shadow-md shadow-cyan-500 hover:text-cyan-500 hover:scale-105 ease-in duration-500 font-semibold">
               View Projects
               <span>
                 <HiArrowNarrowRight className="ml-3 " />
