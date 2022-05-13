e4da81fc2e89779644767fe36f0ea20245dddbb1
JSubba <prolearn1000@protonmail.com>
2022-05-13 20:07:07 +0900

    modifications made

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,14 +15,14 @@ const Home = () => {
         <h2 className="text-6xl sm:text-10xl font-bold py-4">
           I'm a Web Developer
         </h2>
-        <p className="py-4 text-1xl max-w-md">
+        <p className="py-4 text-1xl sm:text-2xl max-w-md">
           I’m a front-end web developer specializing in building and designing
           exceptional digital experiences. At present, I’m focused on building
           responsive front-end web applications.
         </p>
         <div>
           <Link to="projects" smooth={true} duration={500}>
-            <button className="px-10 py-6 my-2 text-1xl flex items-center shadow-md shadow-cyan-500 hover:text-cyan-500 hover:scale-105 ease-in duration-500 font-semibold">
+            <button className="px-10 py-6 my-2 text-1xl sm:text-2xl flex items-center shadow-md shadow-cyan-500 hover:text-cyan-500 hover:scale-105 ease-in duration-500 font-semibold">
               View Projects
               <span>
                 <HiArrowNarrowRight className="ml-3 " />
