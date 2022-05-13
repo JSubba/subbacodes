1f2a351734065fb63005f2b8d3ee86c1ebaf3ac5
JSubba <prolearn1000@protonmail.com>
2022-05-13 18:40:43 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-8xl shadow-md shadow-cyan-500">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "570px",
+      md: "760px",
+      lg: "990px",
+      xl: "1200px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
